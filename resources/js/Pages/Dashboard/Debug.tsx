297cb0824afabb7cf1efb2ad78673e88926fc443
Farhan <animationfar@gmail.com>
2023-12-07 23:37:17 +0700

    feat: make debug can call

--- resources/js/Pages/Dashboard/Debug.tsx
+++ resources/js/Pages/Dashboard/Debug.tsx
@@ -1,13 +1,31 @@
-import { Alert, AlertIcon, Button, Card, CardBody, HStack, Input, VStack } from "@chakra-ui/react";
+import { Alert, AlertIcon, Button, Card, CardBody, HStack, Input, VStack, useToast } from "@chakra-ui/react";
 import { useForm } from "@inertiajs/react";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import DashboardLayout, { DashboardMenu } from "~/Components/Layouts/DashboardLayout";
 import { type UserData } from "~/Components/Layouts/Layout";
 
 export default function Debug() {
-  const { setData, post, errors, recentlySuccessful, wasSuccessful, processing, ...props } = useForm<{ cmd: string; }>();
+  const toast = useToast();
 
-  console.log({ errors, recentlySuccessful, wasSuccessful, processing, ...props })
+  const { setData, post, errors, recentlySuccessful, wasSuccessful, processing, data, ...props } = useForm<{ cmd: string; }>();
+
+  console.log({ errors, recentlySuccessful, wasSuccessful, processing, data, ...props });
+
+  useEffect(() => {
+    if (Object.keys(errors).length > 0) {
+      console.log("Debug", errors);
+      toast({
+        description: "Command Call Error",
+        status: "error",
+      });
+    } else if ( wasSuccessful ) {
+      toast({
+        title: "Command called was successful",
+        description: JSON.stringify(data, null, 2),
+        status: "success",
+      })
+    }
+  }, [errors, recentlySuccessful, wasSuccessful, processing]);
 
   return (<>
     <Card>
@@ -17,14 +35,17 @@ export default function Debug() {
           <AlertIcon />
           This is for development purpose. Please stay away from this panel whenever you do not know what you are doing.
         </Alert>
+
         <HStack mb={8}>
           <Input variant="filled" name="cmd" placeholder="input command" onChange={(e) => setData("cmd", e.target.value)} />
-          <Button variant='ghost'>Call</Button>
+          <Button variant='ghost' onClick={() => post("/dashboard/debug")}>Call</Button>
         </HStack>
+
         <HStack gap={4}>
           <Button variant='outline'>Migrate</Button>
           <Button variant='outline'>Seed</Button>
         </HStack>
+
       </CardBody>
     </Card>
   </>);
