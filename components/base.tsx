b440692c653bf07a612f0990023ff62b1d4290f9
Farhan <animationfar@gmail.com>
2024-02-07 02:34:18 +0700

    feat: template reset password

---
+++ components/base.tsx
@@ -0,0 +1,76 @@
+import {
+    Body,
+    Button,
+    Container as REContainer,
+    Head,
+    Heading as REHeading,
+    Hr,
+    Html,
+    Link,
+    Preview,
+    Text as REText,
+    type HeadingProps,
+    type TextProps,
+  } from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
+import * as React from "react";
+
+export const Heading = (props: HeadingProps) => <REHeading className="text-black text-[12px] font-bold text-center p-0 my-[30px] mx-0" {...props} />;
+export const Text = (props: TextProps) => <REText className="text-black text-[14px] leading-[24px]" {...props} />;
+export const TextMute = (props: TextProps) => <Text className="text-[#666666] text-[12px] leading-[24px]" {...props} />;
+
+export type NodeProps = {
+  children?: React.ReactNode;
+};
+
+export const Header = () => (
+  <Heading>
+    PEMERINTAH DAERAH KHUSUS IBUKOTA JAKARTA<br />
+    KECAMATAN KEMBANGAN KELURAHAN KEMBANGAN UTARA RW 3<br />
+    <br />
+    PEMILIHAN KETUA RT 6
+  </Heading>
+);
+
+export function Footnote({ children }: NodeProps) {
+  return (<>
+    <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+    { children }
+  </>)
+}
+
+export const Footer = () => (
+  <REContainer className="border border-solid border-[#eaeaea] rounded mb-[40px] mx-auto p-[20px] max-w-[465px]">
+    Footer
+  </REContainer>
+);
+
+export function Container({ username, previewText, children, footnote }: {
+  username: React.ReactNode;
+  previewText: string;
+  footnote?: React.ReactNode;
+} & NodeProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview>{previewText}</Preview>
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans px-2">
+          <REContainer className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
+            <Header />
+            <Text>
+              Halo {username},
+            </Text>
+            { children }
+            <Text>
+              Salam Hangat,<br />
+              Panita Pemungutan Suara RT 6
+            </Text>
+            { footnote }
+          </REContainer>
+          <Footer />
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
---
+++ emails/rt6rw3-forget-password.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Container, Footnote, Text, TextMute } from "../components/base";
+import { Button, Link, Section } from "@react-email/components";
+
+export const VercelInviteUserEmail = () => {
+  const previewText = `Ubah Password`;
+
+  return (
+    <Container previewText={previewText} username="User Name"
+      footnote={
+        <Footnote>
+          <TextMute>
+            Jika kamu memiliki masalah dengan tombol "Ubah Password", gunakan atau salin tautan berikut:<br />
+            <Link href={`https://`} className="text-blue-600 no-underline">
+              https://
+            </Link>
+          </TextMute>
+        </Footnote>
+      }
+    >
+      <Text className="text-black text-[14px] leading-[24px]">
+        Kami menerima permintaan Ubah Password terhadap akun <b>NOMOR_NIK</b> dengan email <b>username@example.com</b>. 
+        Jika kamu merasa membuat permintaan ini, kamu dapat menekan tombol "Ubah Password" di bawah ini.
+      </Text>
+      <Section className="text-center mt-[32px] mb-[32px]">
+        <Button
+          className="bg-red-500 rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
+          href="https://"
+        >
+          Ubah Password
+        </Button>
+      </Section>
+      <Text className="text-black text-[14px] leading-[24px]">
+        Permintaan ini hanya berlaku selama 60 menit. Jika kamu merasa tidak pernah membuat permintaan ini, kamu dapat 
+        mengabaikan email. Kamu juga dapat meminta bantuan panitia dengan menghubungi nomor yang tertera pada bagian 
+        terakhir email ini.
+      </Text>
+    </Container>
+  );
+};
+
+export default VercelInviteUserEmail;
